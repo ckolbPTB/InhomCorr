c316252a4e645542400bb59d12edbc1d97b073b1
johannesmayer <johannes.mayer@ptb.de>
2023-04-25 12:55:55 +0200

    Changed location of torch import

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import torch
 
 import inhomcorr.interfaces.mr_data_interface as interface
@@ -7,12 +8,18 @@ import inhomcorr.interfaces.mr_data_interface as interface
 
 class TestImageData(unittest.TestCase):
 
-    def getRandom2DData(self):
-        self.numPixels = 16
-        self.dataShape = (1, 1, self.numPixels, self.numPixels)
-        return torch.rand(self.dataShape)
+    def getRandomPixelNumber(self, nmax=32):
+        return np.random.randint(1, nmax)
+
+    def getRandom2DShape(self):
+        npix = self.getRandomPixelNumber()
+        return (1, 1, npix, npix)
+
+    def getRandom2DData(self, shape):
+        return torch.rand(shape)
 
     def test_2Dshape(self):
         dummy = interface.ImageData()
-        dummy.data = self.getRandom2DData()
-        self.assertEqual(dummy.shape, self.dataShape)
+        shape = self.getRandom2DShape()
+        dummy.data = self.getRandom2DData(shape)
+        self.assertEqual(dummy.shape, shape)
