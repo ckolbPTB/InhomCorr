c774254d4710dc63ee1091300269a93739eb3476
Felix Zimmermann <fzimmermann89@gmail.com>
2023-04-25 12:55:39 +0200

    Add BiasEstimator Interface

---
+++ src/inhomcorr/interfaces/__init__.py
@@ -0,0 +1,5 @@
+from .bias_estimator_interface import BiasEstimator
+from .bias_estimator_interface import HyperParameters
+from .mr_data_interface import ImageData
+from .mr_data_interface import MRData
+from .mr_data_interface import QMRIData
