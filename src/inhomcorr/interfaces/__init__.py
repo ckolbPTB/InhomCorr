6bc963f91b178dcde7d5c0cb9d8ddfb33cb96a69
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-04-25 13:09:44 +0200

    add MRSig and MRParam interfaces

---
+++ src/inhomcorr/interfaces/__init__.py
@@ -0,0 +1,4 @@
+from .mr_data_interface import ImageData as ImageData
+from .mr_data_interface import QMRIData as QMRIData
+from .mr_sig_interface import MRParam as MRParam
+from .mr_sig_interface import MRSig as MRSig
