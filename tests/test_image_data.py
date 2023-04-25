fa30df8a74b0adaa10f6005aaaf396f6d2066229
David Schote <david.schote@ptb.de>
2023-04-25 16:26:31 +0200

    Fix import in test

--- tests/test_image_data.py
+++ tests/test_image_data.py
@@ -2,16 +2,28 @@
 import unittest
 
 from inhomcorr.interfaces.mr_data_interface import ImageData
-
-from .testdata import TestData
+from tests.testdata import TestData
 
 
 class TestImageData(unittest.TestCase):
 
     def setUp(self):
         self.data = TestData()
+        self.shape = self.data.shape
 
     def test_2D_shape(self):
         dummy = ImageData()
         dummy.data = self.data.get_random_tensor()
-        self.assertEqual(dummy.shape, self.data.dim)
+        self.assertEqual(dummy.shape, self.shape)
+
+    def test_set_mask(self):
+        dummy = ImageData()
+        dummy_mask = torch.randint(1, self.shape)
+        dummy.mask = dummy_mask
+        torch.testing.assert_close(dummy.mask, dummy_mask)
+
+    def test_set_header(self):
+        dummy = ImageData()
+        dummy_header = {'test': 8956}
+        dummy.header = dummy_header
+        self.assertEqual(dummy.header['test'], dummy_header['test'])
