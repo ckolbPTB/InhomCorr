78b127e3a47af3b5eb78e0c32be7eb52b5b1dff2
Felix Zimmermann <fzimmermann89@gmail.com>
2023-05-02 16:51:07 +0200

    Initial support for different devices. WIP

--- src/inhomcorr/interfaces/mr_data_interface.py
+++ src/inhomcorr/interfaces/mr_data_interface.py
@@ -1,15 +1,24 @@
 """MRData Interface."""
+from __future__ import annotations
+
+from abc import ABC
+from abc import abstractmethod
+from typing import TypeVar
+
 import numpy as np
 import torch
 
+TMRData = TypeVar('TMRData', bound='MRData')
 
-class MRData():
-    """Basic MR Data Class."""
+
+class MRData(ABC):
+    """Base MR Data Class."""
 
     def __init__(self) -> None:
         self._header: dict = {}
         self._mask: torch.Tensor | None = None
         self._shape: tuple[int, int, int, int] = (1, 1, 1, 1)
+        self._device: torch.device | None = None
 
     @property
     def header(self) -> dict:
@@ -80,17 +89,72 @@ class MRData():
             )
         self._mask = value
 
+    @property
+    def device(self) -> torch.device:
+        """Get device the object resides on.
+
+        Returns
+        -------
+            the device
+        """
+        if self._device is None:
+            return torch.device('cpu')
+        else:
+            return self._device
+
+    @abstractmethod
+    def to(self: TMRData, *, device: str | torch.device) -> TMRData:
+        """Move the object to a device."""
+        pass
+
+    def cpu(self: TMRData) -> TMRData:
+        """Move Object to CPU. Returns a copy.
+
+        Returns
+        -------
+            A copy of the object on the cpu
+        """
+        return self.to(device='cpu')
+
+    def cuda(self: TMRData, device=None) -> TMRData:
+        """Move Object to GPU. Returns a copy.
+
+        Parameters
+        ----------
+            device (Optional): The device to move to. Must be a GPU.
+
+        Returns
+        -------
+            A copy of the object on the cpu
+        """
+        if device is None:
+            device = torch.device('cuda')
+        else:
+            if device.type != 'cuda':
+                raise ValueError('device must be a cuda device')
+        return self.to(device=device)
+
+    def clone(self: TMRData) -> TMRData:
+        """Create a copy of the object.
+
+        Returns
+        -------
+            Copy of the object
+        """
+        return self.to(device=self.device)
+
 
 class ImageData(MRData):
     """Image Data Class."""
 
     def __init__(self, data: torch.Tensor) -> None:
         super().__init__()
-        self._data: torch.Tensor | None = None
+        self._data: torch.Tensor = torch.tensor([])
+        self._device = data.device
         self.data = data
 
     @property
-    def data(self) -> torch.Tensor | None:
+    def data(self) -> torch.Tensor:
         """Getter for data.
 
         Returns
@@ -121,7 +185,7 @@ class ImageData(MRData):
                 'for the parameter which is not broadcastable to current'
                 f'shape {self._shape}'
             )
-        self._data = value
+        self._data = value.to(device=self.device)
 
     @property
     def numpy(self) -> np.ndarray:
@@ -145,6 +209,24 @@ class ImageData(MRData):
             raise AttributeError('Data not defined.')
         return self._data.numpy(force=True)
 
+    def to(self, device: torch.device | str) -> ImageData:
+        """Move to a device, always returns a copy.
+
+        Parameters
+        ----------
+        device:
+            Target device
+
+
+        Returns
+        -------
+            a copy of the ImageData object moved to the device
+        """
+        new = ImageData(self.data.to(device=device))
+        if self._mask is not None:
+            new._mask = self._mask.to(device)
+        return new
+
 
 class QMRIData(MRData):
     """QMRI Data Class.
@@ -158,8 +240,8 @@ class QMRIData(MRData):
         super().__init__()
 
         # Set initial class attributes
-        self._t1: torch.Tensor | None = None
-        self._rho: torch.Tensor | None = None
+        self._t1 = torch.tensor([])
+        self._rho = torch.tensor([])
 
         # Use setters to set t1 and rho
         self.t1 = t1
@@ -195,6 +277,11 @@ class QMRIData(MRData):
         """
         if value is None:
             value = torch.tensor(float('inf')).reshape((1, 1, 1, 1))
+        else:
+            if self._device is None:
+                self._device = value.device
+            else:
+                value = value.to(self._device)
         try:
             self._shape = torch.broadcast_shapes(value.shape, self._shape)
         except RuntimeError:
@@ -216,7 +303,7 @@ class QMRIData(MRData):
         return torch.broadcast_to(self._rho, self._shape)
 
     @rho.setter
-    def rho(self, value: torch.Tensor) -> None:
+    def rho(self, value: torch.Tensor | None) -> None:
         """Setter of the rho map.
 
         Parameters
@@ -231,7 +318,13 @@ class QMRIData(MRData):
         """
         if value is None:
             # Defaults to (1, 1, 1, 1) Tensor with value 1.
-            value = torch.tensor(1.).reshape((1, 1, 1, 1))
+            value = torch.tensor(1.).reshape(
+                (1, 1, 1, 1)).to(device=self.device)
+        else:
+            if self._device is None:
+                self._device = value.device
+            else:
+                value = value.to(self._device)
         try:
             self._shape = torch.broadcast_shapes(value.shape, self._shape)
         except RuntimeError:
@@ -241,3 +334,19 @@ class QMRIData(MRData):
                 f'shape {self._shape}'
             )
         self._rho = value
+
+    def to(self, device: str | torch.device) -> QMRIData:
+        """Return a copy on the specified device.
+
+        Parameters
+        ----------
+        device
+            Target device
+
+        Returns
+        -------
+            a copy on the device
+        """
+        new = QMRIData(t1=self.t1.to(device=device),
+                       rho=self.rho.to(device=device))
+        return new
