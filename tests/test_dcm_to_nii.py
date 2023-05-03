ebbec03877195f910a57723db86c55446a44c3a7
Christoph Aigner <christoph.aigner@tugraz.at>
2023-05-03 15:34:46 +0200

    Cleanup of unused qmri loader and path fix.

--- tests/test_dcm_to_nii.py
+++ tests/test_dcm_to_nii.py
@@ -1,8 +1,9 @@
 """Data loader tests."""
 import os
 import unittest
-
-from inhomcorr.data_loading.dcm2nii import convert_dcm2nii_dir_single_slices
+from pathlib import Path
+from inhomcorr.data_loader.dcm2nii import convert_dcm2nii_dir_single_slices
+import nibabel
 
 # from inhomcorr.data_loading.dcm2nii import convert_dcm2nii_dir
 
@@ -11,48 +12,14 @@ class TestDCM2NII(unittest.TestCase):
 
     def setUp(self):
         # Get working dir
-        self.tmp_path_1dcm = '.\\tests\\TestDicoms_Phantom1Dicom'
-        # self.tmp_path_multidcm = R"C:\Users\aigner01\Documents\Kooperationen\
-        # Hackathon\InhomCorr\tests\TestDicoms_PhantomMultiDicom"
+        self.tmp_path_1dcm = Path('tests')/'TestDicoms_Phantom1Dicom'
 
     def test_1dcm2nii(self):
         # call DCM2NII  and save the results
         convert_dcm2nii_dir_single_slices(
             self.tmp_path_1dcm, self.tmp_path_1dcm)
 
-        # get all the files in the tmp_path_1dcm
-        files = [f for f in os.listdir(self.tmp_path_1dcm)
-                 if os.path.isfile(os.path.join(self.tmp_path_1dcm, f))]
-
-        nii_file = 'None'
-        # check if there is a nii file in the folder
-        for file in files:
-            fileName, fileExtension = os.path.splitext(file)
-            if fileExtension == '.nii':
-                nii_file = fileName + fileExtension
-
-        # check if there is a nii file in there
-        filepathhelper = self.tmp_path_1dcm + '\\'
-        check_file = os.path.exists(filepathhelper + nii_file)
-        self.assertEqual(check_file, True)
-
-    # # this test would be used to test for entire directory
-    # def test_multidcm2nii(self):
-    #     # call DCM2NII  and save the results
-    #     convert_dcm2nii_dir(self.tmp_path_multidcm, self.tmp_path_multidcm)
-
-    #     # get all the files in the tmp_path_multidcm
-    #     files = [f for f in os.listdir(self.tmp_path_multidcm)
-    #              if os.path.isfile(os.path.join(self.tmp_path_multidcm, f))]
-
-    #     nii_file = 'None'
-    #     # check if there is a nii file in the folder
-    #     for file in files:
-    #         fileName, fileExtension = os.path.splitext(file)
-    #         if fileExtension == '.nii':
-    #             nii_file = fileName + fileExtension
-
-    #     # check if there is a nii file in there
-    #     filepathhelper = self.tmp_path_multidcm + "\\"
-    #     check_file = os.path.exists(filepathhelper + nii_file)
-    #     self.assertEqual(check_file, True)
+        files = list(self.tmp_path_1dcm.glob('*.nii'))
+        self.assertEqual(len(files), 1)
+        nii = nibabel.load(files[0])
+        self.assertEqual(nii.shape, (1, 512, 512))
